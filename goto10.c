4ca091e31742fa14219940531cb772849e0f7b89
benjesuis <32913438+benjesuis@users.noreply.github.com>
2018-02-26 01:31:23 -0500

    Upload goto10.c

---
+++ goto10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char pickSlash(void);
+
+int main(void) {
+  srand(time(NULL));
+  char chr;
+
+  for (int i = 0; i < 2000; i++) {
+    chr = pickSlash();
+    printf("%c", chr);
+  }
+  printf("\n");
+
+  return 0;
+}
+
+char pickSlash(void) {
+  char slash;
+
+  int num = rand() % 100 + 1;
+  if (num < 50) {
+    slash = '//';
+  } else {
+    slash = '\\';
+  }
+  return slash;
+}
