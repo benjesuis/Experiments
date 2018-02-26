be6b1f43b484213e08065aedaab2c96b45dd7e0c
benjesuis <32913438+benjesuis@users.noreply.github.com>
2018-02-26 01:55:17 -0500

    Update goto10.c

--- goto10.c
+++ goto10.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 char pickSlash(void);
+char pickLine(void);
 
 int main(void) {
   srand(time(NULL));
@@ -10,6 +11,7 @@ int main(void) {
 
   for (int i = 0; i < 10000; i++) {
     chr = pickSlash();
+    //chr = pickLine();
     printf("%c", chr);
   }
   printf("\n");
@@ -27,4 +29,20 @@ char pickSlash(void) {
     slash = '\\';
   }
   return slash;
+}
+
+char pickLine(void) {
+  char line;
+  
+  int num = rand() % 150 + 1;
+  if (num <= 100) {
+    if (num < 50) {
+      line = '//';
+    } else {
+      line = '\\';
+    }
+  } else {
+    line = '|';
+  }
+  return line;
 }
